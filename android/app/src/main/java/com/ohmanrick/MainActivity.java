b3fd2ed4b847a0ce5314d92b449519f0441df031
Francesco-Voto <francesco.voto.8@gmail.com>
2019-12-20 23:55:28 +0100

    Add cold start to Android

--- android/app/src/main/java/com/ohmanrick/MainActivity.java
+++ android/app/src/main/java/com/ohmanrick/MainActivity.java
@@ -12,4 +12,10 @@ public class MainActivity extends ReactActivity {
   protected String getMainComponentName() {
     return "OhManRick";
   }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    setTheme(R.style.AppTheme);
+    super.onCreate(savedInstanceState);
+  }
 }
